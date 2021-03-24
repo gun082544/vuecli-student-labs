b9c5ae3fdaf55fab4eb60a7cbb882f529f8aaca9
gun082544 <gun082544@hotmail.com>
2021-03-24 12:28:33 +0700

    [UPDATE] tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+const colors = require('tailwindcss/colors')
 module.exports = {
   purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors:{
+        salmon: '#F7A399',
+        red: colors.red,
+        blue: colors.blue,
+        green: colors.green
+    }
   },
   variants: {
     extend: {},
